a580879df7784b18c375067955d70711e333c273
u-shen <farah.ushen@gmail.com>
2025-07-14 16:51:36 +0100

    added active links in js and added fill animations in menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 // Start Header
-const othersLinks = document.querySelector("header nav > ul > li:last-child a");
+
+const allNavLinks = document.querySelectorAll(
+  "header .container nav > ul > li > a",
+);
+const othersLinks = document.querySelector(
+  "header .container nav > ul > li:last-child a",
+);
 const menu = document.querySelector(".menu");
+allNavLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    allNavLinks.forEach((link) => link.classList.remove("active-link"));
+    e.currentTarget.classList.toggle("active-link");
+  });
+});
 othersLinks.addEventListener("click", () => {
   menu.classList.toggle("toggle-show");
 });
