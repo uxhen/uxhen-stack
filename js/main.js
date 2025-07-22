a4cca9a98846fcf2ad26433de804ebf4cd1c8494
u-shen <farah.ushen@gmail.com>
2025-07-22 15:59:44 +0100

    feat(scrolling): add scroll to top and animated width on scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Header Events
 // Start Header
 const allNavLinks = document.querySelectorAll(
   "header .container nav > ul > li > a",
@@ -20,4 +21,30 @@ window.onclick = function (e) {
     menu.classList.remove("toggle-show");
   }
 };
-// End Header
+
+// Scroll Events
+// Scroll To Top
+let scrollToTop = document.querySelector(".scroll-to-top");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > document.documentElement.scrollHeight / 2) {
+    scrollToTop.classList.add("show");
+  } else {
+    scrollToTop.classList.remove("show");
+  }
+});
+scrollToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+// Start width-on-scroll
+window.addEventListener("scroll", () => {
+  const documentYSize = document.documentElement.scrollHeight;
+  const viewportYSize = document.documentElement.clientHeight;
+  const scrollYSize = window.scrollY;
+  const height = documentYSize - viewportYSize;
+  document.querySelector(".width-on-scroll").style.width =
+    `${(scrollYSize / height) * 100}%`;
+});
